4d2665fc262581777c2bb9d5659fa7fce839684f
Sammy Metref <sammy.metref@univ-grenoble-alpes.fr>
2018-03-20 17:42:04 +0100

    20180320-17h42-creationplotDAoutputs

--- Scripts/2018-03-15-sm-qgsw-DI-master-modified/notebooks/Observations.py
+++ Scripts/2018-03-15-sm-qgsw-DI-master-modified/notebooks/Observations.py
@@ -5,17 +5,18 @@ import netCDF4 as nc
 import numpy as np
 
 
-def ObservationCheck(time1,time2):
+def ObservationCheck(time1,deltat,observationpath):
     """
     NAME 
         ObservationCheck 
 
     DESCRIPTION
-        ObservationCheck verifies if observations are available between time1 and time2
+        ObservationCheck verifies if observations are available in [time1-deltat,time1+deltat]
 
         Args:
-            time1 (datetime object): first time 
-            time2 (datetime object): second time 
+            time1 (datetime object): current time 
+            deltat (datetime.timedelta object): data assimilation cycle time  
+            observationpath (string): path to observations
 
         Returns:
             observations_available (boolean): Informs if observations are available between time1 and time2  
